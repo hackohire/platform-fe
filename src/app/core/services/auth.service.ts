262b98e05ba421dcefe1b266aa0aa2836ff41002
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-07-08 11:24:12 +0530

    loggedIn User in LocalStorage

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Hub, ICredentials } from '@aws-amplify/core';
+import { Hub, ICredentials, AWS } from '@aws-amplify/core';
 import { AmplifyService } from 'aws-amplify-angular';
 import { UserService } from 'src/app/user/user.service';
 import { State, Store } from '@ngrx/store';
@@ -8,6 +8,8 @@ import { CreateUser, SetLoggedInUser } from '../store/actions/user.actions';
 import { Router } from '@angular/router';
 import { selectLoggedInUser } from '../store/selectors/user.selector';
 import { User } from 'src/app/shared/models/user.model';
+import { CognitoIdentityServiceProvider } from 'aws-sdk/clients/all';
+import { Auth } from 'aws-amplify';
 
 
 @Injectable()
@@ -28,7 +30,7 @@ export class AuthService {
       this.loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     }
 
-    Hub.listen('auth',(data) => {
+    Hub.listen('auth', (data) => {
       const { channel, payload } = data;
       const state = {
         state: payload.event,
@@ -40,10 +42,9 @@ export class AuthService {
       }
     });
 
-    this.amplifyService.authState().subscribe(authState => {
+    this.amplifyService.authStateChange$.subscribe(authState => {
       console.log('authState', authState);
-      // console.log(authState);
-      // console.log(authState.user.signInUserSession.idToken.jwtToken);
+
       this.authStatus = authState.state;
       this.signedIn = this.authStatus === 'signedIn';
       switch (this.authStatus) {
@@ -52,12 +53,47 @@ export class AuthService {
             this.store.dispatch(new SetLoggedInUser(null));
           } else {
             // Once the user is signed In, Set the token in localstorage as "idToken"
-            localStorage.setItem('idToken', authState.user.signInUserSession.idToken.jwtToken);
+            // localStorage.setItem('idToken', authState.user.signInUserSession.idToken.jwtToken);
             // this.loggedInUser = authState.user;
 
-            // Update set with Loggedin User
-            this.store.dispatch(new CreateUser(authState.user.attributes));
             // this.router.navigateByUrl(`/user/edit`);
+
+            // Add registered user by default in the 'Developers' group in userpool
+            let config = null;
+
+            Auth.currentCredentials().then(async (credentials: ICredentials) => {
+              // console.log(credentials);
+              // Constructor for the global config.
+              config = new AWS.Config({
+                'credentials': credentials,
+                region: 'us-east-1'
+              });
+              const cognitoIdentityServiceProvider = await new CognitoIdentityServiceProvider(config);
+              const params = {
+                GroupName: 'Developer',
+                UserPoolId: authState.user.pool.userPoolId,
+                Username: authState.user.username,
+              };
+              await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
+
+              await Auth.currentAuthenticatedUser({
+                bypassCache: true
+              }).then((user) => {
+                // console.log(user);
+                // Update set with Loggedin User
+                const userToBeSent: User = user.attributes;
+                const token = user.getSignInUserSession().getIdToken().payload;
+                userToBeSent.roles = (token && token['cognito:groups']) ? token['cognito:groups'] : [];
+
+                if (this.getUserFromLocalStorage()) {
+                  this.store.dispatch(new SetLoggedInUser(this.getUserFromLocalStorage()));
+                } else {
+                  this.store.dispatch(new CreateUser(userToBeSent));
+                }
+
+              })
+                .catch(err => console.log(err));
+            });
           }
           break;
 
@@ -65,17 +101,23 @@ export class AuthService {
           break;
 
         case 'signedOut':
-          localStorage.removeItem('idToken');
           this.store.dispatch(new SetLoggedInUser(null));
           this.router.navigateByUrl('/');
       }
     });
 
     this.store.select(selectLoggedInUser).subscribe((u: User) => {
-      localStorage.setItem('loggedInUser', JSON.stringify(u));
+      this.setUserForLocalStorage(u);
       this.loggedInUser = u;
     });
   }
 
+  setUserForLocalStorage(user) {
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+  }
 
+  getUserFromLocalStorage() {
+    const loggedInUser = localStorage.getItem('loggedInUser');
+    return loggedInUser ? JSON.parse(loggedInUser) : '';
+  }
 }
