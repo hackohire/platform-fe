d825b970b746074f73924f9e42fc3e79c4d0b1c2
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-07-08 11:21:04 +0530

    role access directive logic

---
+++ src/app/shared/directives/appcanaaccess.directive.ts
@@ -0,0 +1,39 @@
+import { Directive, OnInit, OnDestroy, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+import { PermissionEvents } from 'src/app/core/services/permisson-event.service';
+import { Subscription } from 'rxjs/internal/Subscription';
+
+@Directive({
+    selector: '[appCanAccess]'
+})
+export class CanAccessDirective implements OnInit, OnDestroy {
+    @Input() appCanAccess: string | string[];
+    private permission$: Subscription;
+
+    constructor(private templateRef: TemplateRef<any>,
+                private viewContainer: ViewContainerRef,
+                private permissionEvents: PermissionEvents) {
+    }
+
+    ngOnInit(): void {
+        this.applyPermission();
+    }
+
+    private applyPermission(): void {
+        this.permission$ = this.permissionEvents
+            .checkAuthorization(this.appCanAccess)
+            .subscribe(authorized => {
+                if (authorized) {
+                    this.viewContainer.clear();
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                } else {
+                    this.viewContainer.clear();
+                }
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.permission$.unsubscribe();
+    }
+
+}
