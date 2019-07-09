678b0fd7176781fffb4129c42a84545997f8603a
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-07-09 19:14:32 +0530

    Prod URL pointing to Backend Changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  graphql_url: 'https://3k18rxkjn5.execute-api.ap-south-1.amazonaws.com/dev/graphql',
+  graphql_url: 'https://qs3c3z6r40.execute-api.ap-south-1.amazonaws.com/prod/graphql',
   s3BucketName: 'platformfile-dev',
   s3BucketURL: 'https://platformfile-dev.s3.amazonaws.com/public/',
   permissions: require('../permissions.json')
